9cd711cbb9a9258add90d9b24ff5b8b821231b64
shinuhyun <shinuhyun@gmail.com>
2021-08-26 16:26:07 +0900

    JS: countDown

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 'use strict';
 
-// play-btn을 누르면
 const playBtn = document.querySelector('.play-btn');
 const section = document.querySelector('section');
 const carrots = document.querySelectorAll('.carrot');
 const bugs = document.querySelectorAll('.bug');
+const time = document.querySelector('.time');
+const replay = document.querySelector('.replay');
+const number = document.querySelector('.number');
 
 const sectionWidth = section.getBoundingClientRect().width;
 const sectionHeight = section.getBoundingClientRect().height;
@@ -13,13 +15,16 @@ let maxX = sectionWidth - 100;
 let maxY = sectionHeight - 100;
 
 playBtn.addEventListener('click', (event) => {
-  // 1. 화면 아래쪽에 bug 7, carrot 10개 랜덤배치
   randomPosition(carrots);
   randomPosition(bugs);
+
   // 2. play-btn이 stop-btn으로 바뀜.
   playBtn.innerHTML = '<i class="fas fa-stop"></i>';
+
+  countDown();
 });
 
+// 1. 화면 아래쪽에 bug 7, carrot 10개 랜덤배치
 function randomPosition(carrots) {
   carrots.forEach((carrot) => {
     carrot.style.visibility = 'visible';
@@ -35,7 +40,26 @@ function getRandomArbitrary(min, max) {
 }
 
 // 3. time에서 10초부터 거꾸로 카운트 다운
+function countDown() {
+  let seconds = 9;
+  let interval = setInterval(() => {
+    time.textContent = `0:${seconds}`;
+    seconds--;
+    if (seconds < 0) {
+      replay.style.visibility = 'visible';
+      replay.clearInterval(interval);
+    }
+  }, 1000);
+}
+
 // 4. number에서 10개를 나타내고, carrot이 줄어들 때마다 줄어듬.
+section.addEventListener('click', (e) => {
+  if (e.target.classList.contains('carrot')) {
+    let beforeNum = number.textContent;
+    number.textContent = beforeNum - 1;
+    e.target.style.visibility = 'hidden';
+  }
+});
 
 // STOP, LOST, WON
 // 1. stop-btn을 누르면
