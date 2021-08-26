e0d1d5f49d5ccbf420447b4dd7191c2b14b2325b
shinuhyun <shinuhyun@gmail.com>
2021-08-26 17:43:53 +0900

    JS: Add carrot, bug click event / replayBtn

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const section = document.querySelector('section');
 const carrots = document.querySelectorAll('.carrot');
 const bugs = document.querySelectorAll('.bug');
 const time = document.querySelector('.time');
-const replay = document.querySelector('.replay');
 const number = document.querySelector('.number');
+const replay = document.querySelector('.replay');
+const replayBtn = document.querySelector('.replay-btn');
+const replayMessage = document.querySelector('.replay-message');
 
 const sectionWidth = section.getBoundingClientRect().width;
 const sectionHeight = section.getBoundingClientRect().height;
@@ -14,17 +16,56 @@ const sectionHeight = section.getBoundingClientRect().height;
 let maxX = sectionWidth - 100;
 let maxY = sectionHeight - 100;
 
-playBtn.addEventListener('click', (event) => {
+playBtn.addEventListener('click', () => {
+  countDown();
+
   randomPosition(carrots);
   randomPosition(bugs);
 
-  // 2. play-btn이 stop-btn으로 바뀜.
   playBtn.innerHTML = '<i class="fas fa-stop"></i>';
+  playBtn.classList.replace('play-btn', 'stop-btn');
+  document.querySelector('.stop-btn').addEventListener('click', (e) => {
+    e.currentTarget.style.visibility = 'hidden';
+    e.currentTarget.classList.replace('stop-btn', 'play-btn');
+    replay.style.visibility = 'visible';
+    replayMessage.textContent = 'REPLAY?';
+  });
+});
+
+section.addEventListener('click', (e) => {
+  if (e.target.classList.contains('carrot')) {
+    let beforeNum = number.textContent;
+    if (beforeNum === '1') {
+      replay.style.visibility = 'visible';
+    }
+    number.textContent = beforeNum - 1;
+    e.target.style.visibility = 'hidden';
+  }
+  if (e.target.classList.contains('bug')) {
+    replay.style.visibility = 'visible';
+    replayMessage.textContent = 'YOU LOST!';
+  }
+});
+
+replayBtn.addEventListener('click', () => {
+  playBtn.style.visibility = 'visible';
+  replay.style.visibility = 'hidden';
+
+  randomPosition(carrots);
+  randomPosition(bugs);
+
+  playBtn.innerHTML = '<i class="fas fa-stop"></i>';
+  playBtn.classList.replace('play-btn', 'stop-btn');
+  document.querySelector('.stop-btn').addEventListener('click', (e) => {
+    e.currentTarget.style.visibility = 'hidden';
+    e.currentTarget.classList.replace('stop-btn', 'play-btn');
+    replay.style.visibility = 'visible';
+    replayMessage.textContent = 'REPLAY?';
+  });
 
   countDown();
 });
 
-// 1. 화면 아래쪽에 bug 7, carrot 10개 랜덤배치
 function randomPosition(carrots) {
   carrots.forEach((carrot) => {
     carrot.style.visibility = 'visible';
@@ -39,35 +80,15 @@ function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-// 3. time에서 10초부터 거꾸로 카운트 다운
 function countDown() {
   let seconds = 9;
   let interval = setInterval(() => {
     time.textContent = `0:${seconds}`;
     seconds--;
     if (seconds < 0) {
+      clearInterval(interval);
       replay.style.visibility = 'visible';
-      replay.clearInterval(interval);
+      replayMessage.textContent = 'YOU LOST!';
     }
   }, 1000);
 }
-
-// 4. number에서 10개를 나타내고, carrot이 줄어들 때마다 줄어듬.
-section.addEventListener('click', (e) => {
-  if (e.target.classList.contains('carrot')) {
-    let beforeNum = number.textContent;
-    number.textContent = beforeNum - 1;
-    e.target.style.visibility = 'hidden';
-  }
-});
-
-// STOP, LOST, WON
-// 1. stop-btn을 누르면
-//    시간이 0초가 되거나, bug를 클릭하면
-//    당근을 모두 찾으면
-// 2. stop-btn 보이지 않고,
-// 3. replay가 보이고, 상황에 해당하는 replay-message가 보임.
-// 4. time이 멈춤.
-
-// replay-btn을 누르면
-// play-btn 누른 것과 같음.
